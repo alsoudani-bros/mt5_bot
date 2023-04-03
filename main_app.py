f05d9b94d0d7bf21a2b7d4e79714062dbb01875f
hayderahmad@yahoo.com <hayderahmad@yahoo.com>
2023-04-03 07:23:30 -0600

    more changes

--- main_app.py
+++ main_app.py
@@ -17,14 +17,14 @@ from time import sleep
 register_matplotlib_converters()
 config = dotenv_values(".env")
 
-# login = config.get("MT_LOGIN")
-# password = config.get("MT_PASSWORD")
+login = config.get("MT_LOGIN")
+password = config.get("MT_PASSWORD")
 # # server = "MetaQuotes-Demo"
-# server="FTMO-Demo"
+server="FTMO-Demo"
 
-login = config.get("CHALLANGE_MT_LOGIN")
-password = config.get("CHALLANGE_MT_PASSWORD")
-server="FTMO-Server"
+# login = config.get("CHALLANGE_MT_LOGIN")
+# password = config.get("CHALLANGE_MT_PASSWORD")
+# server="FTMO-Server"
 
 handlers.establish_MT5_connection(
     login, server, password)
@@ -160,6 +160,27 @@ def check_market(symbol, time_frame, stage_one_risk_percent, stage_two_risk_perc
 def still_alive():
     pass
 
+# def check_market_callback():
+#     break_end_hour = 17
+#     break_end_minute = 0
+#     risk_reward_ratio = 3
+#     if symbol != "US100.cash":
+#         break_end_hour = 17
+#         break_end_minute = 0
+#         risk_reward_ratio = 1
+#     check_market(symbol=symbol,
+#                 time_frame="15min",
+#                 stage_one_risk_percent=0.1,
+#                 stage_two_risk_percent=0.2,
+#                 stages_cut_profit_percent=2.5,
+#                 risk_reward_ratio=risk_reward_ratio,
+#                 starting_balance_for_the_week=200000,
+#                 break_start_hour=13,
+#                 break_start_minute=30,
+#                 break_end_hour=break_end_hour,
+#                 break_end_minute=break_end_minute,
+#                 max_positions_open_at_once_per_direction=10)
+    
 def check_market_callback():
     break_end_hour = 23
     break_end_minute = 59
