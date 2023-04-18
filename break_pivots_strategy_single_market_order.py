a2b21515351514db928a002c9fcda3594cace95a
hayderahmad@yahoo.com <hayderahmad@yahoo.com>
2023-04-18 09:24:27 -0600

    more changes

--- break_pivots_strategy_single_market_order.py
+++ break_pivots_strategy_single_market_order.py
@@ -35,6 +35,16 @@ def check_market(time_frame, risk_percent, risk_reward_ratio,start_break_hour, s
             
             
         if not break_time:
+            if not handlers.position_still_open(symbol, last_long_position_ticket):
+                last_long_position_pivot_high = 0.0
+                last_long_position_pivot_low = 0.0
+                last_long_position_ticket = 0
+            
+            if not handlers.position_still_open(symbol, last_short_position_ticket):
+                last_short_position_pivot_high = 0.0
+                last_short_position_pivot_low = 0.0
+                last_short_position_ticket = 0
+                
             current_balance = handlers.get_balance()
             recent_pivot_high = handlers.get_pivot_highs(symbol, time_frame, 100, 2)[0]
             recent_pivot_low = handlers.get_pivot_lows(symbol, time_frame, 100, 2)[0]
@@ -47,10 +57,8 @@ def check_market(time_frame, risk_percent, risk_reward_ratio,start_break_hour, s
             print(f"last candle open: {last_candle_open} and close: {last_candle_close}")
             
             if not handlers.reached_max_loss(starting_balance, current_balance, 2):
-                there_is_no_long_position_still_open_at_current_pivot_high = last_long_position_pivot_high != recent_pivot_high or (last_long_position_pivot_high == recent_pivot_high and not handlers.position_still_open(symbol, last_long_position_ticket))
-                there_is_no_short_position_still_open_at_current_pivot_low = last_short_position_pivot_low != recent_pivot_low or (last_short_position_pivot_low == recent_pivot_low and not handlers.position_still_open(symbol, last_short_position_ticket))
                 
-                if last_candle_close > recent_pivot_high and last_candle_low <= recent_pivot_high and last_candle_close > last_candle_open and last_long_position_pivot_low != recent_pivot_low and there_is_no_long_position_still_open_at_current_pivot_high:
+                if last_candle_close > recent_pivot_high and last_candle_low <= recent_pivot_high and last_candle_close > last_candle_open and last_long_position_pivot_low != recent_pivot_low and last_long_position_pivot_high != recent_pivot_high:
                     print("Start taking a long position")
                     stop_loss = recent_pivot_low
                     if handlers.send_market_order(symbol, "long", stop_loss, risk_reward_ratio, risk_percent):
@@ -58,7 +66,7 @@ def check_market(time_frame, risk_percent, risk_reward_ratio,start_break_hour, s
                         last_long_position_pivot_low = recent_pivot_low
                         last_long_position_ticket = handlers.get_most_recent_position(symbol).ticket
                 
-                elif last_candle_close < recent_pivot_low and last_candle_high >= recent_pivot_low and  last_candle_close < last_candle_open and last_short_position_pivot_high != recent_pivot_high and there_is_no_short_position_still_open_at_current_pivot_low:
+                elif last_candle_close < recent_pivot_low and last_candle_high >= recent_pivot_low and  last_candle_close < last_candle_open and last_short_position_pivot_high != recent_pivot_high and last_short_position_pivot_low != recent_pivot_low:
                     print("start taking a short position")
                     stop_loss = recent_pivot_high
                     if handlers.send_market_order(symbol, "short", stop_loss, risk_reward_ratio, risk_percent):
