48c828e4c30ff353462f40f466072438e24c26a5
hayderahmad@yahoo.com <hayderahmad@yahoo.com>
2023-04-18 10:28:31 -0600

    more changes

--- break_pivots_strategy_single_market_order.py
+++ break_pivots_strategy_single_market_order.py
@@ -2,27 +2,21 @@ import handlers
 import datetime
 
 last_long_position_pivot_high = 0.0
-last_long_position_pivot_low = 0.0
 last_long_position_ticket = 0
-last_short_position_pivot_high = 0.0
 last_short_position_pivot_low = 0.0
 last_short_position_ticket = 0
 starting_balance = 0.0
 last_report_time = datetime.datetime.now()
 symbol= input("Enter the symbol you want to trade: ").strip()
 
-
 def check_market(time_frame, risk_percent, risk_reward_ratio,start_break_hour, start_break_minute, end_break_hour, end_break_minute):
     global symbol
     global last_long_position_pivot_high
-    global last_long_position_pivot_low
     global last_long_position_ticket
-    global last_short_position_pivot_high
     global last_short_position_pivot_low
     global last_short_position_ticket
     global starting_balance
     global last_report_time
-    
         
     if not handlers.is_weekend_break():
         break_time = handlers.The_time_of(start_break_hour,start_break_minute,end_break_hour,end_break_minute, "Break time")
@@ -37,40 +31,38 @@ def check_market(time_frame, risk_percent, risk_reward_ratio,start_break_hour, s
         if not break_time:
             if not handlers.position_still_open(symbol, last_long_position_ticket):
                 last_long_position_pivot_high = 0.0
-                last_long_position_pivot_low = 0.0
                 last_long_position_ticket = 0
             
             if not handlers.position_still_open(symbol, last_short_position_ticket):
-                last_short_position_pivot_high = 0.0
                 last_short_position_pivot_low = 0.0
                 last_short_position_ticket = 0
                 
             current_balance = handlers.get_balance()
             recent_pivot_high = handlers.get_pivot_highs(symbol, time_frame, 100, 2)[0]
             recent_pivot_low = handlers.get_pivot_lows(symbol, time_frame, 100, 2)[0]
+            fast_recent_pivot_high = handlers.get_pivot_highs(symbol, time_frame, 100, 1)[0]
+            fast_recent_pivot_low = handlers.get_pivot_lows(symbol, time_frame, 100, 1)[0]
             last_candle = handlers.get_candles_by_count(symbol, time_frame, 1)
             last_candle_open = last_candle['open'][0]
             last_candle_close = last_candle['close'][0]
             last_candle_high = last_candle['high'][0]
             last_candle_low = last_candle['low'][0]
             
-            print(f"last candle open: {last_candle_open} and close: {last_candle_close}")
+            print(f"\n\n Report.............................. \n\n last candle open: {last_candle_open}\n last candle close: {last_candle_close}\n last candle high: {last_candle_high}\n last candle low: {last_candle_low} \n recent pivot high: {recent_pivot_high}\n recent pivot low: {recent_pivot_low}\n fast recent pivot high: {fast_recent_pivot_high}\n fast recent pivot low: {fast_recent_pivot_low}\n last long position pivot high: {last_long_position_pivot_high}\n last short position pivot low: {last_short_position_pivot_low}\n\n End of Report..............................\n\n")
             
             if not handlers.reached_max_loss(starting_balance, current_balance, 2):
                 
-                if last_candle_close > recent_pivot_high and last_candle_low <= recent_pivot_high and last_candle_close > last_candle_open and last_long_position_pivot_low != recent_pivot_low and last_long_position_pivot_high != recent_pivot_high:
+                if last_candle_close > recent_pivot_high and last_candle_low <= recent_pivot_high and last_candle_close > last_candle_open and last_long_position_pivot_high != recent_pivot_high:
                     print("Start taking a long position")
-                    stop_loss = recent_pivot_low
+                    stop_loss = fast_recent_pivot_low if fast_recent_pivot_low > recent_pivot_low else recent_pivot_low    
                     if handlers.send_market_order(symbol, "long", stop_loss, risk_reward_ratio, risk_percent):
                         last_long_position_pivot_high = recent_pivot_high
-                        last_long_position_pivot_low = recent_pivot_low
                         last_long_position_ticket = handlers.get_most_recent_position(symbol).ticket
                 
-                elif last_candle_close < recent_pivot_low and last_candle_high >= recent_pivot_low and  last_candle_close < last_candle_open and last_short_position_pivot_high != recent_pivot_high and last_short_position_pivot_low != recent_pivot_low:
+                elif last_candle_close < recent_pivot_low and last_candle_high >= recent_pivot_low and  last_candle_close < last_candle_open and last_short_position_pivot_low != recent_pivot_low:
                     print("start taking a short position")
-                    stop_loss = recent_pivot_high
+                    stop_loss = fast_recent_pivot_high if fast_recent_pivot_high < recent_pivot_high else recent_pivot_high
                     if handlers.send_market_order(symbol, "short", stop_loss, risk_reward_ratio, risk_percent):
-                        last_short_position_pivot_high = recent_pivot_high
                         last_short_position_pivot_low = recent_pivot_low
                         last_short_position_ticket = handlers.get_most_recent_position(symbol).ticket
                             
